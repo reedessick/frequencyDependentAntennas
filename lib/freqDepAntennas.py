857c28ff34d28a3a5a671ea47ed775c572455ded
Reed Essick <reed.essick@ligo.org>
2017-04-13 12:52:32 -0700

    finished debugging frequency dependent antenna pattern functions as well as a script that plots them and checks it for sanity

--- lib/freqDepAntennas.py
+++ lib/freqDepAntennas.py
@@ -21,63 +21,56 @@ def __dV__( q, exp_twoFreqsT ):
     helper function that determines the prefactor for the antenna patterns.
     this may typically be included in the PSD rather than the antenna patterns, so we may want to set this to 1
     '''
-    return exp_twoFreqsT/(1-q*exp_twoFreqsT)
+    return 0.5
+#    return exp_twoFreqsT/(1-q*exp_twoFreqsT)
 
 def __D__( freqsT, twoFreqsT, exp_twoFreqsT, N ):
     '''
     helper function that returns the part of the frequency dependence that depends on the arm's directions
     '''
-    if isinstance(freqsT, (int, float)):
+    if isinstance(freqsT, (int, float, complex)):
         if freqsT==0:
-            return 1
+            return np.ones_like(N, dtype=complex)
 
     elif np.all(freqsT==0):
-        return 1
+        return np.ones((len(freqsT), len(N)), dtype=complex)
 
-    a = 1-N
-    b = 1+N
+    a = 1-np.outer(np.ones_like(freqsT), N)
+    b = 2-a
 
-    return ((1-np.exp(-a*twoFreqsT))/a - exp_twoFreqsT*(1-np.exp(b*freqsT))/b)/twoFreqsT
+    freqsT = np.outer(freqsT, np.ones_like(N))
+    twoFreqsT = np.outer(twoFreqsT, np.ones_like(N))
+    exp_twoFreqsT = np.outer(exp_twoFreqsT, np.ones_like(N))
+
+    ### assemble things carefully so we don't have to worry about nans later
+    ans = np.zeros_like(a)
+
+    truth = twoFreqsT==0 ### this kills all terms
+    ans[truth] = 1
+
+    truth = np.logical_not(truth) ### flip it so it's everything else, use this to further filter array assignment
+
+    # where a is non-zero
+    this_truth = truth*(a!=0)
+    ans[this_truth] += (1-np.exp(-a[this_truth]*freqsT[this_truth]))/(a[this_truth]*twoFreqsT[this_truth])
+
+    # wehre a is zero
+    this_truth = truth*(a==0)
+    ans[this_truth] += 0.5
+
+    # where b is non-zero
+    this_truth = truth*(b!=0)
+    ans[this_truth] -= exp_twoFreqsT[this_truth]*(1-np.exp(b[this_truth]*freqsT[this_truth]))/(b[this_truth]*twoFreqsT[this_truth])
+
+    # where b is zero
+    this_truth = truth*(b==0)
+    ans[this_truth] -= 0.5*exp_twoFreqsT[this_truth]
+
+    return ans
 
 #--- actual responses
 
-def antenna_response( theta, phi, psi, q, T=1., freqs=0. ):
-    '''
-    the input variables mean things
-    theta, phi, psi, and freqs should all be the same length np.ndarray objects if they are not floats
-
-    return F+, Fx
-    '''
-    ### compute trigonometric functions only once!
-    cosTheta = np.cos(theta)
-    sinTheta = np.sin(theta)
-
-    cosPhi = np.cos(phi)
-    sinPhi = np.sin(phi)
-
-    cos2psi = np.cos(2*psi)
-    sin2psi = np.cos(2*psi)
-
-    ### define relevant coordinate unit vectors for convenience
-    nx = sinTheta*cosPhi
-    ny = sinTheta*sinPhi
-    
-    ### compute terms common to each antenna pattern
-    freqsT = 2j*np.pi*freqs*T
-    twoFreqsT = 2*freqsT
-    exp_twoFreqsT = np.exp(-twoFreqsT)
-
-    dV = __dV__(q, exp_twoFreqsT)
-    dV_xx = __D__(freqsT, twoFreqsT, exp_twoFreqsT, nx)
-    dV_yy = __D__(freqsT, twoFreqsT, exp_twoFreqsT, ny)
-
-    ### assemble the derivatives
-    dV_dhp = dV*(dV_xx*(nx**2 - sinPhi**2) - dV_yy*(ny**2 - cosPhi**2))
-    dV_dhx = -dV*(dV_xx + dV_yy)*cosTheta*2*sinPhi*cosPhi
-
-    return dV_dhp*cos2psi - dV_dhc*sin2psi, dV_dhp*sin2psi + dV_dhc*cos2psi
-
-def coordIndep_antenna_response( theta, phi, psi, ex, ey, q, T=1., freqs=0. ):
+def antenna_response( theta, phi, psi, ex, ey, q, T=1., freqs=0. ):
     '''
     the input variables mean things
     theta, phi, psi, and freqs should all be the same length np.ndarray objects if they are not floats
@@ -89,15 +82,13 @@ def coordIndep_antenna_response( theta, phi, psi, ex, ey, q, T=1., freqs=0. ):
     ### compute the trigonometric functions only once
     cosTheta = np.cos(theta)
     sinTheta = np.sin(theta)
-
     cosPhi = np.cos(phi)
     sinPhi = np.sin(phi)
-
     cosPsi = np.cos(psi)
     sinPsi = np.sin(psi)
 
     ### compute unit vectors in Earth-Fixed frame from the wave-frame
-    ex_wave = np.array([sinPhi*cosPsi - sinPsi*cosPhi*cosTheta, -cosPhi*cosPsi - sinPsi*sinPhi*cosTheta, sinPhi*sinTheta])
+    ex_wave = np.array([sinPhi*cosPsi - sinPsi*cosPhi*cosTheta, -cosPhi*cosPsi - sinPsi*sinPhi*cosTheta, sinPsi*sinTheta])
     ey_wave = np.array([-sinPhi*sinPsi - cosPsi*cosPhi*cosTheta, cosPhi*sinPsi - cosPsi*sinPhi*cosTheta, sinTheta*cosPsi])
 
     ### compute cartesian vector for the line-of-sight to the source
@@ -109,16 +100,57 @@ def coordIndep_antenna_response( theta, phi, psi, ex, ey, q, T=1., freqs=0. ):
     exp_twoFreqsT = np.exp(-twoFreqsT)
 
     dV = __dV__(q, exp_twoFreqsT)
-    dV_yy = dV * __D__(freqsT, twoFreqsT, exp_twoFreqsT, np.sum(n*ey))
-    dV_xx = dV * __D__(freqsT, twoFreqsT, exp_twoFreqsT, np.sum(n*ex))
+    dV_xx = dV * __D__(freqsT, twoFreqsT, exp_twoFreqsT, np.sum(np.outer(ex, np.ones_like(theta))*n, axis=0))
+    dV_yy = dV * __D__(freqsT, twoFreqsT, exp_twoFreqsT, np.sum(np.outer(ey, np.ones_like(theta))*n, axis=0))
 
     ### assemble these parts into antenna responses
     Fp = 0.
     Fx = 0.
     for i in xrange(3): ### FIXME? may be able to do this more efficiently vi np.array manipulations?
+        exi = ex[i]
+        eyi = ey[i]
+        ex_wavei = ex_wave[i]
+        ey_wavei = ey_wave[i]
         for j in xrange(3):
-            detector = dV_xx * ex[i]*ex[j] - dV_yy * ey[i]*ey[j] ### compute matrix element for detector matrix
-            Fp += detector * (ex_wave[i]*ex_wave[j] - ey_wave[i]*ey_wave[j]) ### multiply by matrix element from wave polarizations
-            Fx += detector * (ex_wave[i]*ey_wave[j] + ey_wave[i]*ex_wave[j])
+            ex_wavej = ex_wave[j]
+            ey_wavej = ey_wave[j]
+            Dij = dV_xx * exi*ex[j] - dV_yy * eyi*ey[j] ### compute matrix element for detector matrix
+            Fp += Dij * (ex_wavei*ex_wavej - ey_wavei*ey_wavej) ### multiply by matrix element from wave polarizations
+            Fx += Dij * (ex_wavei*ey_wavej + ey_wavei*ex_wavej)
+
+    return Fp, Fx
+
+#--- from bayesburst (known to be correct, supported for cross-validation purposes)
+
+def const_antenna_response(theta, phi, psi, nx, ny):
+    """
+    computes the antenna patterns for detector arms oriented along nx and ny (cartesian vectors). 
+
+    Antenna patterns are computed accoring to Eqn. B7 from Anderson, et all PhysRevD 63(04) 2003
+    """
+    cos_theta = np.cos(theta)
+    sin_theta = np.sin(theta)
+    cos_phi = np.cos(phi)
+    sin_phi = np.sin(phi)
+    cos_psi = np.cos(psi)
+    sin_psi = np.sin(psi)
+
+    X = (sin_phi*cos_psi - sin_psi*cos_phi*cos_theta, -cos_phi*cos_psi - sin_psi*sin_phi*cos_theta, sin_psi*sin_theta)
+    Y = (-sin_phi*sin_psi - cos_psi*cos_phi*cos_theta, cos_phi*sin_psi - cos_psi*sin_phi*cos_theta, sin_theta*cos_psi)
+
+    ### iterate over x,y,z to compute F+ and Fx
+    Fp = 0.
+    Fx = 0.
+    for i in xrange(3):
+        nx_i = nx[i]
+        ny_i = ny[i]
+        Xi = X[i]
+        Yi = Y[i]
+        for j in xrange(3):
+            Xj = X[j]
+            Yj = Y[j]
+            Dij = 0.5*(nx_i*nx[j] - ny_i*ny[j])
+            Fp += (Xi*Xj - Yi*Yj)*Dij
+            Fx += (Xi*Yj + Yi*Xj)*Dij
 
     return Fp, Fx
